f771efa704a094d2a4dd68564a8693fa71d7233d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-10-03 17:43:18 +0530

    :zap: Implemented Teams and projects component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
-  <v-app class="black darken-4">
+  <v-app>
     <Navbar />
-
-    <v-main>
-      <v-content>
-        <router-view></router-view>
-      </v-content>
-    </v-main>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import Navbar from './components/Navbar';
-
+import Navbar from "./components/Navbar";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    Navbar
+    Navbar,
   },
 
   data: () => ({
