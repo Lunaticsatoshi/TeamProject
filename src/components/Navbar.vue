1968c076a62047e6923d17a8f98209f1ebfb2333
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-10-03 18:00:04 +0530

    :zap: Implemented Projects and Teams Component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,52 +1,51 @@
 <template>
-    <nav>
-        <v-toolbar flat app>
-            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title class="text-uppercase grey--text">
-                <span class="font-weight-regular">Team</span>
-                <span class="font-weight-black">Project</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-                
-            </v-toolbar-items>
-            <v-btn rounded color="button">
-                    <span>Logout</span>
-                    <v-icon right>chevron_right</v-icon>
-            </v-btn>
-        </v-toolbar>
-        <v-navigation-drawer  absolute temporary v-model="drawer" class="primary">
-            <v-list>
-                <v-list-item-group v-model="items">
-                    <v-list-item :key="i" v-for="(item, i) in items" link :to="item.route">
-                        <v-list-item-icon>
-                            <v-icon left class="white--text" v-text="item.icon"></v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title class="white--text" v-text="item.text"></v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list-item-group>
-            </v-list>
-        </v-navigation-drawer>
-    </nav>
+  <nav>
+    <v-toolbar flat app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-regular">Team</span>
+        <span class="font-weight-black">Project</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items> </v-toolbar-items>
+      <v-btn rounded color="button">
+        <span>Logout</span>
+        <v-icon right>chevron_right</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer absolute temporary v-model="drawer" class="primary">
+      <v-list nav dense v-model="items">
+        <v-list-item :key="item.text" v-for="item in items" :to="item.route">
+          <v-list-item-icon>
+            <v-icon left class="white--text" v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title
+              class="white--text"
+              v-text="item.text"
+            ></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                drawer: false,
-                items: [
-                    {icon: 'dashboard', text: 'Home', route: '/'},
-                    {icon: 'folder', text: 'Projects', route: '/projects'},
-                    {icon: 'group', text: 'Teams', route: '/teams'}
-                ]
-            }
-        }
-    }
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { icon: "dashboard", text: "Home", route: "/" },
+        { icon: "folder", text: "Projects", route: "/projects" },
+        { icon: "group", text: "Teams", route: "/teams" },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
